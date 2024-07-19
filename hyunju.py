1dd8d3eea6eb8c6539eeb95640bfa6eb9fc9c7c1
hyeonzuu <shj0909hj@gmail.com>
2024-07-19 10:41:47 +0900

    0719

--- hyunju.py
+++ hyunju.py
@@ -8,13 +8,12 @@ plt.rcParams['axes.unicode_minus'] =False
 df = pd.read_excel("./pre_data/pre_select.xlsx")
 df
 
-
 df_2018 = df.loc[0:14]
 df_2019 = df.loc[15:29]
 df_2020 = df.loc[30:44]
 df_2021 = df.loc[45:59]
 df_2022 = df.loc[60:74]
-
+df_2018.dtypes()
 
 total_2018 = df_2018.sort_values("total", ascending = False).head()
 total_2019 = df_2019.sort_values("total", ascending = False).head()
@@ -32,14 +31,34 @@ plt.ylabel('Total')
 plt.legend(title='Item', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.xticks(rotation=0)  # x축 레이블을 수평으로 설정
 plt.show()
+plt.clf()
+---------------------------------------------------------------------------------
 
-#볼거리 제공 
+#볼거리 제공 선 그래프
 visualappeal = total_all.query("item == '볼거리 제공'")
-visualappeal.polt.bar(rot = 0)
+Popularity = total_all.query("item == '여행지 지명도'")
+time = total_all.query("item == '여행할 수 있는 시간'")
+
+
+# 선 그래프 생성
+plt.figure(figsize=(10, 6))
+plt.plot(visualappeal["year"], visualappeal["total"], marker='o',label="볼거리 제공")
+plt.plot(Popularity["year"], Popularity["total"], marker='o',label="여행지 지명도")
+plt.plot(time["year"], time["total"], marker='o',label="여행할 수 있는 시간")
+
+
+# 그래프에 제목과 축 레이블 추가
+plt.title("여행을 선택하는 이유 - 전체")
+plt.xlabel("년도")
+plt.ylabel("전체 %")
+plt.legend(fontsize='x-small',loc='center left')
 plt.show()
+---------------------------------------------------------------------------------
+# 남녀의 여행 선택 차이
+male_all = df[["male", "item"]]
+female_all = df[["female", "item"]]
 
-
-
+male_2018 = df_2018[["item","male"]].sort_index("item")
 
 
 
--- preprocessing/preprocessing2_hj.py
+++ preprocessing/preprocessing2_hj.py
@@ -20,11 +20,56 @@ df["teens"] = df["15~19세"]
 df["young_adults"] = df["20대"] + df["30대"]
 df["middle_adults"] = df["40대"] + df["50대"]
 df["senior"] = df["60대"] + df["70세 이상"]
+#2018
+df.loc[0:14, "young_adults"] = (df.loc[0:14, "young_adults"] / df.loc[0:14, "young_adults"].sum() * 100).round(1)
+df.loc[0:14, "middle_adults"] = (df.loc[0:14, "middle_adults"] / df.loc[0:14, "middle_adults"].sum() * 100).round(1)
+df.loc[0:14, "senior"] = (df.loc[0:14, "senior"] / df.loc[0:14, "senior"].sum() * 100).round(1)
+#2019
+df.loc[15:29, "young_adults"] = (df.loc[15:29, "young_adults"] / df.loc[15:29, "young_adults"].sum() * 100).round(1)
+df.loc[15:29, "middle_adults"] = (df.loc[15:29, "middle_adults"] / df.loc[15:29, "middle_adults"].sum() * 100).round(1)
+df.loc[15:29, "senior"] = (df.loc[15:29, "senior"] / df.loc[15:29, "senior"].sum() * 100).round(1)
+#2020
+df.loc[30:44, "young_adults"] = (df.loc[30:44, "young_adults"] / df.loc[30:44, "young_adults"].sum() * 100).round(1)
+df.loc[30:44, "middle_adults"] = (df.loc[30:44, "middle_adults"] / df.loc[30:44, "middle_adults"].sum() * 100).round(1)
+df.loc[30:44, "senior"] = (df.loc[30:44, "senior"] / df.loc[30:44, "senior"].sum() * 100).round(1)
+#2021
+df.loc[45:59, "young_adults"] = (df.loc[45:59, "young_adults"] / df.loc[45:59, "young_adults"].sum() * 100).round(1)
+df.loc[45:59, "middle_adults"] = (df.loc[45:59, "middle_adults"] / df.loc[45:59, "middle_adults"].sum() * 100).round(1)
+df.loc[45:59, "senior"] = (df.loc[45:59, "senior"] / df.loc[45:59, "senior"].sum() * 100).round(1)
+#2022
+df.loc[60:74, "young_adults"] = (df.loc[60:74, "young_adults"] / df.loc[60:74, "young_adults"].sum() * 100).round(1)
+df.loc[60:74, "middle_adults"] = (df.loc[60:74, "middle_adults"] / df.loc[60:74, "middle_adults"].sum() * 100).round(1)
+df.loc[60:74, "senior"] = (df.loc[60:74, "senior"] / df.loc[60:74, "senior"].sum() * 100).round(1)
+
+
 df = df.drop(['15~19세', '20대', '30대', '40대', '50대', '60대', '70세 이상'], axis=1)
+
 # 월급
 df["l_sal"] = df["100만원 미만"] + df["100~200만원 미만"]
 df["m_sal"] = df["200~300만원 미만"] + df["300~400만원 미만"] + df["400~500만원 미만"]
 df["h_sal"] = df["500~600만원 미만"] + df["600만원 이상"]
+#2018
+df.loc[0:14, "l_sal"] = (df.loc[0:14, "l_sal"] / df.loc[0:14, "l_sal"].sum() * 100).round(1)
+df.loc[0:14, "m_sal"] = (df.loc[0:14, "m_sal"] / df.loc[0:14, "m_sal"].sum() * 100).round(1)
+df.loc[0:14, "h_sal"] = (df.loc[0:14, "h_sal"] / df.loc[0:14, "h_sal"].sum() * 100).round(1)
+#2019
+df.loc[15:29, "l_sal"] = (df.loc[15:29, "l_sal"] / df.loc[15:29, "l_sal"].sum() * 100).round(1)
+df.loc[15:29, "m_sal"] = (df.loc[15:29, "m_sal"] / df.loc[15:29, "m_sal"].sum() * 100).round(1)
+df.loc[15:29, "h_sal"] = (df.loc[15:29, "h_sal"] / df.loc[15:29, "h_sal"].sum() * 100).round(1)
+#2020
+df.loc[30:44, "l_sal"] = (df.loc[30:44, "l_sal"] / df.loc[30:44, "l_sal"].sum() * 100).round(1)
+df.loc[30:44, "m_sal"] = (df.loc[30:44, "m_sal"] / df.loc[30:44, "m_sal"].sum() * 100).round(1)
+df.loc[30:44, "h_sal"] = (df.loc[30:44, "h_sal"] / df.loc[30:44, "h_sal"].sum() * 100).round(1)
+#2021
+df.loc[45:59, "l_sal"] = (df.loc[45:59, "l_sal"] / df.loc[45:59, "l_sal"].sum() * 100).round(1)
+df.loc[45:59, "m_sal"] = (df.loc[45:59, "m_sal"] / df.loc[45:59, "m_sal"].sum() * 100).round(1)
+df.loc[45:59, "h_sal"] = (df.loc[45:59, "h_sal"] / df.loc[45:59, "h_sal"].sum() * 100).round(1)
+#2022
+df.loc[60:74, "l_sal"] = (df.loc[60:74, "l_sal"] / df.loc[60:74, "l_sal"].sum() * 100).round(1)
+df.loc[60:74, "m_sal"] = (df.loc[60:74, "m_sal"] / df.loc[60:74, "m_sal"].sum() * 100).round(1)
+df.loc[60:74, "h_sal"] = (df.loc[60:74, "h_sal"] / df.loc[60:74, "h_sal"].sum() * 100).round(1)
+
+
 df = df.drop(['100만원 미만', '100~200만원 미만', '200~300만원 미만', '300~400만원 미만', '400~500만원 미만',\
                 '500~600만원 미만', '600만원 이상'], axis=1)
 # 가구
